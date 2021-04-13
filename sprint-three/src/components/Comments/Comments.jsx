780892f189d155b6f5e72f70dccbed59d356c1f1
heather-ross <hross299@yahoo.com>
2021-04-13 17:30:56 -0400

    initial sprint 3 commit. Updated component names and adjust video thumbnail to min and max width

---
+++ sprint-three/src/components/Comments/Comments.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Comments.scss';
+
+function Comments({currentVideo}) {
+    
+    return (
+        <div className="comments__stream">  
+          {currentVideo && currentVideo.sort(function(x, y) {return y.timestamp - x.timestamp;})
+          .map(comment => { 
+            const commentDate = new Date(comment.timestamp);
+            return (
+              <div className="comments__container" key={comment.id}>
+               <div className="avatar comments__avatar-comment"></div>           
+                <div className="comments__block">
+                  <h5 className="comments__block--name">{comment.name}</h5>
+                  <p className="comments__block--date">{commentDate.toLocaleDateString()}</p>
+                  <p className="comments__block--comment">{comment.comment}</p>             
+                </div>
+              </div> 
+            )
+          })}               
+        </div>
+    )
+}
+export default Comments
