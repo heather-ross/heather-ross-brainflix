e9b17e3ddccd1763e1040a1eb6c80f550dd32b6d
heather-ross <hross299@yahoo.com>
2021-04-13 23:26:36 -0400

    basic express set-up

--- sprint-three/client/src/pages/HomePage/Home.jsx
+++ sprint-three/client/src/pages/HomePage/Home.jsx
@@ -6,6 +6,8 @@ import HeroVideo from '../../components/HeroVideo/HeroVideo';
 import VideoDetails from '../../components/VideoDetails/VideoDetails';
 import VideoList from '../../components/VideoList/VideoList';
 
+// const expressUrl = 'http://localhost:8080';
+
 class Home extends React.Component {
     state = { 
         currentVideoId: null,
@@ -15,8 +17,10 @@ class Home extends React.Component {
 
     componentDidMount = () => {
         axios.get('https://project-2-api.herokuapp.com/videos?api_key=013226f6-61a5-4220-9634-2e2e331c2789')
+        // axios.get(expressUrl)
         .then( res1 => {
           axios.get(`https://project-2-api.herokuapp.com/videos/${res1.data[0].id}?api_key=013226f6-61a5-4220-9634-2e2e331c2789`)
+          // axios.get(`${expressUrl}/videos/${res1.data[0].id}`)
           .then( res2 => { 
             this.setState({
               currentVideoId: res2.data.id,
@@ -34,6 +38,7 @@ class Home extends React.Component {
       const id = this.props.match.params.id ? this.props.match.params.id : this.state.nextVideos[0].id;
       if(id !== this.state.currentVideoId) {
         axios.get(`https://project-2-api.herokuapp.com/videos/${id}?api_key=013226f6-61a5-4220-9634-2e2e331c2789`)
+        // axios.get(`${expressUrl}/${id}`)
         .then( res => { 
           this.setState({
             currentVideoId: res.data.id,
@@ -51,6 +56,7 @@ class Home extends React.Component {
         e.preventDefault();
         let addComment = {name: "Heather Ross ", comment: e.target.commentInput.value};
         axios.post(`https://project-2-api.herokuapp.com/videos/${this.state.currentVideoId}/comments?api_key=013226f6-61a5-4220-9634-2e2e331c2789`,addComment)
+        // axios.post(`${expressUrl}/videos/${this.state.currentVideoId}/comments`,addComment)
             .then(res => {
               console.log(res)
               this.setState({
