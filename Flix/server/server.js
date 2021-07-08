7cb207b2c3cf0871978c29747635686472377c72
heather-ross <hross299@yahoo.com>
2021-07-08 15:14:12 -0400

    updated port

--- Flix/server/server.js
+++ Flix/server/server.js
@@ -1,9 +1,8 @@
-require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
 const videos = require('./routes/videos');
-const port = process.env.PORT
+const port = 8080;
 
 app.use(cors());
 app.use(express.json())
